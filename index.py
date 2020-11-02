1795697d32eaccbdff6db667f40eebb39846307f
孙乾栋 <987958223@qq.com>
2020-11-02 09:59:15 +0800

    监听文件名按年月自己计算

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 from dirmonitor import DirMonitor
 import os
+import time
 
 def callback(filepath):
     print(filepath + "has changed!")
 
-dir_path = "C:\\Users\gxrb\Documents\WeChat Files\qq987958223\FileStorage\Image\\2020-10"
-decoded_dir_path = "C:\\Users\\gxrb\\Desktop\\decode\\2020-10"
+mdir = time.strftime("%Y-%m", time.localtime())
+dir_path = "C:\\Users\gxrb\Documents\WeChat Files\qq987958223\FileStorage\Image\\"+mdir
+decoded_dir_path = "C:\\Users\\gxrb\\Desktop\\decode\\"+mdir
 #解码目录不存在则创建
 if not os.path.isdir(decoded_dir_path):
     os.makedirs(decoded_dir_path)
